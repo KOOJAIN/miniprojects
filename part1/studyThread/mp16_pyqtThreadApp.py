2d6f9ba69f46c19a614ffe4c2512672db2f75fa1
KOOJAIN <rnwkdls97@naver.com>
2023-03-15 15:13:15 +0900

    3월15일

--- part1/studyThread/mp16_pyqtThreadApp.py
+++ part1/studyThread/mp16_pyqtThreadApp.py
@@ -6,25 +6,30 @@ from PyQt5.QtGui import * # QIcon은 여기있습
 from PyQt5.QtCore import * # Qt.white...
 import time
 
+MAX = 1000
+
 class BackgroundWorker(QThread): # PyQt5 스레드를 위한 클래스 존재
-    procChanged = pyqtSignal(int)
+    procChanged = pyqtSignal(int) # 커스텀 시그널 (마우스 클릭같은 시그널을 따로 만드는 것)
 
     def __init__(self, count=0,parent=None) -> None:
         super().__init__()
         self.parent = parent
-        self.working = True # 스레드 동작여부
+        self.working = False # 스레드 동작여부
         self.count = count
 
-    def run(self):
+    def run(self): # thread.start() --> run()
         # self.parent.pgbTask.setRange(0, 100)
         # for i in range(0, 101):
         #     print(f'스레드 출력 > {i}')
         #     self.parent.pgbTask.setValue(i)
         #     self.parent.txbLog.append(f'스레드 출력 > {i}')
         while self.working:
-            self.procChanged.emit(self.count) # 시그널을 내보냄
-            self.count += 1 # 값 증가만
-
+            if self.count <= MAX:
+                self.procChanged.emit(self.count) # 시그널을 내보냄
+                self.count += 1 # 값 증가만 // 업무프로세스 동작하는 위치
+                time.sleep(0.001) # 0.0000001 세밀하게 주면 GUI처리를 제대로 못함
+            else:
+                self.working = False
 class qtApp(QWidget):
     def __init__(self):
         super().__init__()
@@ -37,7 +42,7 @@ class qtApp(QWidget):
         self.worker = BackgroundWorker(parent = self, count=0)
         # 백그라운드 워커에 있는 시그널을 접근 슬롯함수
         self.worker.procChanged.connect(self.procUpdated)
-        self.pgbTask.setRange(0, 1000000)
+        self.pgbTask.setRange(0, MAX)
 
     @pyqtSlot(int)
     def procUpdated(self, count):
@@ -45,11 +50,10 @@ class qtApp(QWidget):
         self.pgbTask.setValue(count)
         print(f'스레드 출력 > {count}')
 
-    
-    @pyqtSlot()
     def btnStartClicked(self):
-        self.worker.start()
+        self.worker.start() # 스레드 클래스 run() 실행
         self.worker.working = True
+        self.worker.count = 0 
 
 if __name__ == '__main__' :    
     app = QApplication(sys.argv)
