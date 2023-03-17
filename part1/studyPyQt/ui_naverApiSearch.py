53b098876fa26edd4fa8e8035fc3a42c10aa427c
KOOJAIN <rnwkdls97@naver.com>
2023-03-17 12:26:27 +0900

    .

--- part1/studyPyQt/ui_naverApiSearch.py
+++ part1/studyPyQt/ui_naverApiSearch.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'c:\source\miniprojects\part1\miniprojects\part1\studyPyQt\naverApiSearch.ui'
+# Form implementation generated from reading ui file 'c:\source\miniprojects\part1\miniprojects\miniprojects\part1\studyPyQt\naverApiSearch.ui'
 #
 # Created by: PyQt5 UI code generator 5.15.9
 #
--- part1/studyPython/ui_cominfo.py
+++ part1/studyPython/ui_cominfo.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'c:\source\miniprojects\part1\miniprojects\part1\studyPython\cominfo.ui'
+# Form implementation generated from reading ui file 'c:\source\miniprojects\part1\miniprojects\miniprojects\part1\studyPython\cominfo.ui'
 #
 # Created by: PyQt5 UI code generator 5.15.9
 #
