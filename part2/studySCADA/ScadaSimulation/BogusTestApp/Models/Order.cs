636714e0657ff9613c693b51bf433c88b142e205
KOOJAIN <rnwkdls97@naver.com>
2023-05-02 14:01:40 +0900

    5.2

---
+++ part2/studySCADA/ScadaSimulation/BogusTestApp/Models/Order.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BogusTestApp.Models
+{
+    internal class Order        // 주문 테이블
+    {
+        public Guid Id { get; set; }
+        public DateTime Date { get; set; }
+        public decimal OrderValue { get; set; }
+        public bool Shipped { get; set; }   // 주문 선적여부
+    }
+}
